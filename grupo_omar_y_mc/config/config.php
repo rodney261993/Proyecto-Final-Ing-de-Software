64f460b334058ed4be7b2b6f5fe27278bc4ce9f3
Omar Concepcion <omarconcepcion1002@gmail.com>
2021-03-28 22:03:11 -0500

    Se elimino archivos inncesarios.

--- grupo_omar_y_mc/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'restapi'); 
-//referencia generado con MD5(uniqid(<some_string>, true)) 
-define('API_KEY','3d524a53c110e4c22463b10ed32cef9d');
